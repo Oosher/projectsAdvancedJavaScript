e7348c80668c22fb8c5f4a3762248bfcf2775545
Oosher <105375761+Oosher@users.noreply.github.com>
2022-08-21 18:04:40 +0300

    added some different methods for fetching and element from the html

---
+++ stopwatch/stopWatch.js
@@ -0,0 +1,16 @@
+let counter=0;
+let isRuning =false;
+/* methods isted of -- getElementbyId();
+const view = document.querySelector("#stopWatch");
+console.log(view);
+const p =document.querySelector("p");
+console.log(p);
+const randomClass = document.querySelector(".randomClass");
+console.log(randomClass); */
+
+
+function stopWatch(){
+if (isRuning==false){
+
+}
+}
