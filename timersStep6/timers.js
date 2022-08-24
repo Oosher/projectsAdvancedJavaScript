1ded75c3185f64125dc7cf0608f57ef9f39c7ea1
Oosher <105375761+Oosher@users.noreply.github.com>
2022-08-24 20:57:25 +0300

    added a deleat function

--- timersStep6/timers.js
+++ timersStep6/timers.js
@@ -1,24 +1,37 @@
+
+
 function timer(timerInitialization){
     this.initializeTimer = timerInitialization;
+    let newElement;
+    this.deleteTimer =function(){
+
+        newElement.remove();
+
+    }
+
 
     this.createTimer = function(){
 
-        let timerContainer = document.querySelector("#timerContainer");
+        const timerContainer = document.querySelector("#timerContainer");
 
          /*  timerContainer.innerHTML+="<div>"+this.initializeTimer+"</div>"; */
 
         //creating an element
-        const newElement = document.createElement("div");
-        newElement.innerText="new timer";
+        newElement = document.createElement("div");
+        newElement.innerHTML="new timer"+ this.initializeTimer +" <br>";
 
         const startButton = document.createElement("button");
         startButton.innerText="Start Timer";
 
+        const deleteButton = document.createElement("button");
+        deleteButton.innerText="delete timer";
+
         newElement.appendChild(startButton);
-        
+        newElement.appendChild(deleteButton);
+
         timerContainer.appendChild(newElement);
 
-        console.log("timer inisialzation: ",this.initializeTimer);
+        deleteButton.addEventListener("click",this.deleteTimer)
     }
 
 }
