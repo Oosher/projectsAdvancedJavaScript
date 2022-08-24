d10ea7393465f362f109c1ebafff9fc724b9ac06
Oosher <105375761+Oosher@users.noreply.github.com>
2022-08-24 19:47:36 +0300

    added on click event

--- timersStep4/app.js
+++ timersStep4/app.js
@@ -8,3 +8,17 @@ console.log(timer1);
 
 const timer2 =new timer(888);
 console.log(timer2);
+
+function addTimer(){
+    const initialTime = document.querySelector("#timeInput").value;
+
+
+
+    console.log(initialTime);
+
+}
+
+
+
+
+document.querySelector("#createTimer").addEventListener("click",addTimer);
--- timersStep4/timers.js
+++ timersStep4/timers.js
@@ -8,3 +8,5 @@ function timer(timerInitialization){
 }
 
 export default timer;
+
+
