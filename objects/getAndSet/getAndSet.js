26853a3c5c8978efd46ba83eb40c77db5f531df4
Oosher <105375761+Oosher@users.noreply.github.com>
2022-09-07 18:08:08 +0300

    Update getAndSet.js

--- objects/getAndSet/getAndSet.js
+++ objects/getAndSet/getAndSet.js
@@ -3,6 +3,8 @@ const student ={};
 student.fullname="Yoshi Abukshish";
 student.birthdate ="22/3/2006";
 
-student["city"] = "Jerusalem"
+student["city"] = "Jerusalem";
+
+student["First name"] = "amnon";
 
 console.log(student);
