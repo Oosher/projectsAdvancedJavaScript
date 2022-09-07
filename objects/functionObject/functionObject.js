3c1294b77389f7685214f2676366c53a71b74def
Oosher <105375761+Oosher@users.noreply.github.com>
2022-09-07 18:38:05 +0300

    added a way of crating a constrictor function

--- objects/functionObject/functionObject.js
+++ objects/functionObject/functionObject.js
@@ -1,4 +1,17 @@
 
-function Student(fullname,is,birthDate){
-    this.fullname=fullname;
+function Student(fullname,id,birthDate){
+    this.fullname = fullname;
+    this.id = id;
+    this.birthDate = birthDate;
+    this.sayHello = function(){
+
+        console.log("Hello", this.fullname)
+    }
 }
+
+
+const newStudent = new Student("Amnon Lehi","008","22/8/1994");
+
+console.log(newStudent);
+
+newStudent.sayHello();
