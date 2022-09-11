86c3fc153341436f7ca1191b33428781bd3e91c1
Oosher <105375761+Oosher@users.noreply.github.com>
2022-09-11 17:54:54 +0300

    added some basic JavaScript tutorials

---
+++ javascriptTotorials/js.js
@@ -0,0 +1,38 @@
+//variables :
+
+let firstVar ="Mashu";
+const id="029302930219323";
+
+
+//can be defined twice:
+
+var x=5;
+
+var x=10;
+
+
+
+//examples :
+
+let  number = 10;
+
+let result =0 ;
+
+
+result+= number;
+
+console.log(result);
+
+//operators + - * / % 
+
+
+number = 12389;
+
+//result = 9
+result =number%10;
+
+
+
+//functions 
+
+
