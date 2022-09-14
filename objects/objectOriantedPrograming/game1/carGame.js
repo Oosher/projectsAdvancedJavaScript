f5e28500ee66713ea11d250fff02c81e7d988827
Oosher <105375761+Oosher@users.noreply.github.com>
2022-09-14 20:42:14 +0300

    added movement to the player

--- objects/objectOriantedPrograming/game1/carGame.js
+++ objects/objectOriantedPrograming/game1/carGame.js
@@ -1,10 +1,13 @@
-document.addEventListener("keydown",downMovement);
+document.addEventListener("keydown",playerHandling);
 
+// const car = document.querySelector(".car");
 
-let car = document.querySelector(".car");
-function downMovement(event){
+function playerHandling(event){
 
-    if(event.key=="ArrowDown"){
+    player.movement(event.key);
+}
+
+    /*if(event.key=="ArrowDown"){
         console.log("event.key"); 
         car.style.transform = "rotate(90deg)";
     }
@@ -22,4 +25,89 @@ function downMovement(event){
     }
    
 
+
+ */
+function Car(){
+    this.direction = "arrowRight";
+    this.locationColumn = 1;
+    this.locationRow = 1;
+    this.car = document.querySelector(".car");
+    this.movement = function (pressedKey)
+    {
+    if(pressedKey=="ArrowDown"){
+        this.car.style.transform = "rotate(90deg)";
+        this.locationRow++;
+        this.car.style.gridRow=this.locationRow;
+        
+
+    }
+    if(pressedKey=="ArrowUp"){
+        this.car.style.transform = "rotate(-90deg)";
+        this.locationRow--;
+        this.car.style.gridRow=this.locationRow;
+        
+
+    }
+    if(pressedKey=="ArrowRight"){
+        this.car.style.transform = "rotate(0deg)";
+        this.locationColumn++;
+        this.car.style.gridColumn=this.locationColumn;
+
+    }
+    if(pressedKey=="ArrowLeft"){
+        this.car.style.transform = "scaleX(-1)";
+        this.locationColumn--;
+        this.car.style.gridColumn=this.locationColumn;
+
+    }
+    this.checkEnds();
+    /* this.move(pressedKey); */
+};
+
+this.checkEnds = function (){
+
+    if(this.locationColumn>10 || this.locationColumn<1){
+     
+        this.locationColumn=5;
+        this.car.style.gridColumn=this.locationColumn;
+      
+    }
+
+    if (this.locationRow>10||this.locationRow<1) {
+        console.log("bruh");
+        this.locationRow=5;
+        this.car.style.gridRow=this.locationRow;
+    }
+
+};
+    /* this.move = function (keyPress) {
+
+    if(keyPress=="ArrowDown"){
+        player.locationRow++;
+        player.car.style.gridRow=player.locationRow;
+
+    }
+    if(keyPress=="ArrowUp"){
+        player.locationRow--;
+        player.car.style.gridRow=player.locationRow;
+
+    }
+    if(keyPress=="ArrowRight"){
+        player.locationColumn++;
+        player.car.style.gridColumn=player.locationColumn;
+
+    }
+    if(keyPress=="ArrowLeft"){
+        player.locationColumn--;
+        player.car.style.gridColumn=player.locationColumn;
+
+    }
+  } */
+
+    
+    
 }
+
+
+
+const player = new Car();
