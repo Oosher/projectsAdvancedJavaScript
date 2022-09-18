42fdbafeef3a2fd27cdef7274f475a557ea6f142
Oosher <105375761+Oosher@users.noreply.github.com>
2022-09-18 19:55:06 +0300

    Update studentsObject.js

--- objects/studentsObject/studentsObject.js
+++ objects/studentsObject/studentsObject.js
@@ -20,10 +20,33 @@ function randomStudents(numberOfStudents){
     let students =[];
 
     for (let i = 0; i < numberOfStudents; i++) {
-       students.push({newStudent:new Student("Yoshi",Math.floor(Math.random()*999999999),Math.floor(Math.random()*9999999999)),newGrades: new Grades(Math.floor(Math.random()*100),Math.floor(Math.random()*100),Math.floor(Math.random()*100))});
+       students.push({newStudent:new Student("Yoshi",Math.floor(Math.random()*999999999),Math.floor(Math.random()*9999999999)),newGrades: new Grades(Math.floor(Math.random()*100)+1,Math.floor(Math.random()*100)+1,Math.floor(Math.random()*100)+1)});
         
     }
 
     return students;
 }
+
 console.log(randomStudents(8));
+
+let allStudents=randomStudents(8);
+
+function bestStudent(students){
+    let smartestStudent=students[0];
+    let prevAvarage=0;
+    for (let i = 0; i < students.length; i++) {
+
+        console.log(prevAvarage,(students[i].newGrades.math+students[i].newGrades.history+students[i].newGrades.english)/3);
+
+         if( prevAvarage < ((students[i].newGrades.math+students[i].newGrades.history+students[i].newGrades.english)/3)){
+            smartestStudent=students[i];
+            prevAvarage=(students[i].newGrades.math+students[i].newGrades.history+students[i].newGrades.english)/3;
+         }
+
+        
+        
+    }
+    return smartestStudent;
+}
+console.log(allStudents);
+console.log(bestStudent(allStudents));
