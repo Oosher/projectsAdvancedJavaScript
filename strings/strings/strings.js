7bb4dd595908a8a39f5acd7a6e7d62cfe7a08b64
Oosher <105375761+Oosher@users.noreply.github.com>
2022-09-21 20:17:29 +0300

    added some exampeles about strings

---
+++ strings/strings/strings.js
@@ -0,0 +1,13 @@
+
+// backslash \
+console.log("may name is \"bruh\"");
+console.log("may name is \n bruh");
+console.log("may name is \t bruh");
+
+// interpolation
+
+let x=100 ,y = 200,c = 300;
+
+console.log("the x is :"+x+y+c);
+
+console.log(`the x is : ${x} ${y} ${c}`);
