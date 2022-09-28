f65a1cae8a5d4480e3c58bd2bdca1d8102364256
Oosher <105375761+Oosher@users.noreply.github.com>
2022-09-28 18:38:54 +0300

    added a new way of filtering letters from a string

---
+++ strings/stringsArray2/stringsArray2.js
@@ -0,0 +1,31 @@
+let array=[2,3,4,5,2,4,5,3,5,6,5,4,3,4,5,4,3,4,5,4,3,4,5];
+
+console.log(array.indexOf(5));
+
+
+
+//option 1:
+let stringA="aba halah lavbvoda yvia leosher matana";
+
+removeAFromeTheString(stringA);
+
+function removeAFromeTheString(string){
+
+    let newString =string;
+
+    while (newString.indexOf("a")!=-1) {
+        newString=newString.replace("a","");
+
+    }
+    
+  
+    
+
+    console.log(newString);
+}
+
+
+
+
+
+//option 2:
