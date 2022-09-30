846e31c986fc588f119bba68cdb8dce64b6d5e7f
Oosher <105375761+Oosher@users.noreply.github.com>
2022-09-30 13:55:48 +0300

    Update stringsHW.js

--- strings/stringsHW2/stringsHW.js
+++ strings/stringsHW2/stringsHW.js
@@ -3,6 +3,18 @@
 //1:
 
 
+let numArray=[1,3,44,55,6,14,23,14,553,2,6,567,23,1,3,5,6,7]
+let higherThan10Position= numArray.findIndex(allHigherThan10);
+console.log(higherThan10Position);
+
+
+function allHigherThan10(number,index){
+if (number>10) {
+  return index;
+}
+
+
+}
 
 
 
