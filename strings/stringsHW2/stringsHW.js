e63c9be49e0b7e44f316fac364698168d9e52e30
Oosher <105375761+Oosher@users.noreply.github.com>
2022-10-02 18:40:38 +0300

    added some new options to solve question 1

--- strings/stringsHW2/stringsHW.js
+++ strings/stringsHW2/stringsHW.js
@@ -40,7 +40,7 @@ function allHigherThan10Map(number ,index,array){
 
 }
 
-//option 3 - while :
+//option 3 - while findIndex :
 
 let numArray2 = [1,3,44,55,6,14,23,14,553,2,6,567,23,1,3,5,6,7];
 let tempArray = numArray2;
@@ -54,13 +54,35 @@ while (numArray2.findIndex(ifBiggerThan10)!=-1) {
 
 }
 
-console.log(arrayOfIndex2+" bruhg");
+console.log(arrayOfIndex2);
 console.log("hello");
 
 
 function ifBiggerThan10(num) {
   return num>10;
 }
+
+
+//option 3 filter :
+
+let numArray3 =[1,4,55,66,7,44];
+
+let numArray3Filtered = numArray3.filter(filterBiggerThan10);
+
+findFilteredInOriginalArray(numArray3Filtered,numArray3);
+
+function findFilteredInOriginalArray(filteredArray,originalArray) {
+
+  for (let i = 0; i < filteredArray.length; i++) {
+  console.log(originalArray.indexOf(filteredArray[i]));
+  
+  }
+
+}
+
+ function filterBiggerThan10(number){
+  return number>10;
+ }
 //2:
 
 let arrayOfText = ["sus","big","opos","Shmuel","Shimon","yosef","abrsam","single"]
