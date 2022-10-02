60e5da0a1b9657c3878fd1048ea3b7bfe13efebb
Oosher <105375761+Oosher@users.noreply.github.com>
2022-10-02 18:26:05 +0300

    Update stringsHW.js

--- strings/stringsHW2/stringsHW.js
+++ strings/stringsHW2/stringsHW.js
@@ -4,6 +4,7 @@
 
 // finedIndexאני מבין שצריך להשתמש במה שלמדנו אבל לא ראיתי דרך שנראת הגיונית  וקצרה לעשות אותה בשימוש ב
 
+//option 1 - my way : 
 let numArray = [1,3,44,55,6,14,23,14,553,2,6,567,23,1,3,5,6,7];
 
 console.log(allHigherThan10(numArray));
@@ -22,8 +23,44 @@ return arrayOfIndex;
 
 }
 
+//option 2 - map :
+
+let biggerThan10UsingMap = numArray.map(allHigherThan10Map);
+
+console.log(biggerThan10UsingMap);
 
 
+function allHigherThan10Map(number ,index,array){
+
+  if (number>10) {
+    return index;
+  }
+  
+
+
+}
+
+//option 3 - while :
+
+let numArray2 = [1,3,44,55,6,14,23,14,553,2,6,567,23,1,3,5,6,7];
+let tempArray = numArray2;
+let arrayOfIndex2 = [];
+while (numArray2.findIndex(ifBiggerThan10)!=-1) {
+
+  let Index2 = tempArray.findIndex(ifBiggerThan10);
+  arrayOfIndex2.push(Index2);
+  tempArray[Index2]=0;
+
+
+}
+
+console.log(arrayOfIndex2+" bruhg");
+console.log("hello");
+
+
+function ifBiggerThan10(num) {
+  return num>10;
+}
 //2:
 
 let arrayOfText = ["sus","big","opos","Shmuel","Shimon","yosef","abrsam","single"]
