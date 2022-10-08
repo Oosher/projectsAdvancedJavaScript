4b72d1b493467c23523f583be83d77f34f422126
Oosher <105375761+Oosher@users.noreply.github.com>
2022-10-08 11:59:47 +0300

    added som preper for test questions

---
+++ strings/testQuestions/testQuestions.js
@@ -0,0 +1,63 @@
+function Product(name,serialNumber,sellPrice,buyPrice,stock) {
+
+
+    this.name = name;
+    this.serialNumber = serialNumber;
+    this.sellPrice = sellPrice;
+    this.buyPrice = buyPrice;
+    this.stock = stock;
+
+}
+
+
+function crateArrayOfProducts(n){
+
+    let arrayOfProducts = [];
+
+    for (let i = 0; i < n; i++) {
+       arrayOfProducts.push(new Product("Product "+(i+1),Math.floor(Math.random()*1000000),Math.floor(Math.random()*1000),Math.floor(Math.random()*1000),Math.floor(Math.random()*100)));
+        
+    }
+
+    return arrayOfProducts;
+
+}
+
+let arrayOfProducts = crateArrayOfProducts(5);
+
+console.log(arrayOfProducts);
+
+
+function stockBuyPrice(array) {
+
+    let stockBuy = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        stockBuy+=(array[i].buyPrice * array[i].stock);
+        
+    }
+
+    return stockBuy;
+    
+}
+
+
+
+
+console.log("the Stock buy price is : " + stockBuyPrice(arrayOfProducts));
+
+function stockSellPrice(array) {
+
+    let stockSell = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        stockSell+=(array[i].sellPrice * array[i].stock);
+        
+    }
+
+    return stockSell;
+    
+}
+
+
+console.log("the Stock sell price is : " + stockSellPrice(arrayOfProducts));
