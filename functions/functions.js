2949c580be5273a2ba8a9f29c46187dabb4f9b8b
Oosher <105375761+Oosher@users.noreply.github.com>
2022-10-23 21:09:40 +0300

    added a math object examples and exersise

---
+++ functions/functions.js
@@ -0,0 +1,27 @@
+
+//void functions
+
+
+function sayHello(){
+
+console.log("Hello bruh");
+
+}
+
+
+//return functions 
+
+
+function checkIfEven(number){
+
+    if (number%2==0) {
+        return true;
+    }
+    return false;
+
+}
+
+
+//call function 
+
+//a function that calls another function
