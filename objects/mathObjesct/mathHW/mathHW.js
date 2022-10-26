a2029f9fdc6976f44121b19abb70cbd82656878b
Oosher <105375761+Oosher@users.noreply.github.com>
2022-10-26 17:52:12 +0300

    added some math homework by creating a function that calculates circle's area and hikef

---
+++ objects/mathObjesct/mathHW/mathHW.js
@@ -0,0 +1,20 @@
+
+
+
+console.log(areaOfACircle(20));
+//calculating the area of a circle
+function areaOfACircle(radius){
+
+    return " The area of teh circle is : " +((radius*radius)*Math.PI);
+
+}
+
+
+ console.log(theCircumferenceOfACircle(4));  
+
+function theCircumferenceOfACircle(radius){
+
+
+    return " C = :"+(2*Math.PI*radius)
+
+}
