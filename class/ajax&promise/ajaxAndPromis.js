ff4e8cbe91b3874a7f4da3492c7b6bb4a915b777
Oosher <105375761+Oosher@users.noreply.github.com>
2022-12-04 17:48:43 +0200

    added some new class task about adding some options to the select tag

---
+++ class/ajax&promise/ajaxAndPromis.js
@@ -0,0 +1,35 @@
+
+
+
+const selectTab = document.querySelector("#country");
+
+
+
+function  addOptionToSelectTag(arrayOfOptions) {
+
+
+    arrayOfOptions.map((countryName)=>{
+
+        let newCountry = document.createElement("option")
+
+        newCountry.value=`${countryName}`;
+
+        newCountry.innerHTML=countryName;
+
+        selectTab.appendChild(newCountry);
+    })
+
+    
+}
+
+
+
+
+let arrayOfCountries = ["isr","pol","ger","bruh","mum","us"];
+
+
+
+
+addOptionToSelectTag(arrayOfCountries);
+
+
