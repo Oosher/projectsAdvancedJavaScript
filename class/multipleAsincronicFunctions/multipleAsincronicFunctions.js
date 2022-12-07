c92a2324dfc0b8c4fe08bfd07d51e045fe22962a
Oosher <105375761+Oosher@users.noreply.github.com>
2022-12-07 18:51:16 +0200

    Update multipleAsincronicFunctions.js

--- class/multipleAsincronicFunctions/multipleAsincronicFunctions.js
+++ class/multipleAsincronicFunctions/multipleAsincronicFunctions.js
@@ -5,49 +5,22 @@ const search = document.querySelector("#search");
 
 const searchButton = document.querySelector("#searchButton");
 
-searchButton.addEventListener("click",countryName)
+searchButton.addEventListener("click",()=>{
+
+    countryName(); 
+
+    getWeather();
+    
+}
+)
 
 function countryName() {
 
-    const newPromise = new Promise((resolve,reject)=>{
-
-    const xhttp2 = new XMLHttpRequest();
-
-        xhttp2.open("GET",`https://restcountries.com/v3.1/name/${search.value}?fullText=true`);
-
-        xhttp2.onload = function (){
-
-            console.log(this);
-
-            if (xhttp2.status==200) {
-
-                resolve(xhttp2.response);
-
-            }
-            else{
-
-                reject(xhttp2.status);
-
-            }
-
-    }
-
-
-
-xhttp2.send();
-
-
-
-})
-
-
-
+   
 
 let countryResult;
 
-newPromise
-
-    .then((response=>{
+newPromise.then((response)=>{
 
             countryResult = response;
 
@@ -56,14 +29,14 @@ newPromise
             console.log(countryResult);
 
             
-          
+
             
 
-    }))
+    })
 
     .catch ((status)=>{
 
-        document.body.innerHTML=`ERROR ${status} PAGE NOT FOUND`;
+        alert(`ERROR ${status} COUNTRY DOS NOT EXIST`);
 
     })
 
@@ -108,4 +81,92 @@ newPromise
 
         
     }
-      
+
+
+    function  getWeather() {
+
+    
+    
+            weatherPromise
+                .then((response=>{
+                                let selectedCounter;
+
+                                let weatherObj = response;
+
+                                weatherObj= JSON.parse(weatherObj)
+
+                                console.log(weatherObj);
+
+                }))
+
+                .catch((status)=>{
+
+                        alert(`ERROR ${status} COUNTRY DOS NOT EXIST`);
+
+                })
+
+}
+
+
+
+
+
+
+function moreThanOnePromise() {
+
+    const newPromise = new Promise((resolve,reject)=>{
+
+        const xhttp2 = new XMLHttpRequest();
+
+            xhttp2.open("GET",`https://restcountries.com/v3.1/name/${search.value}?fullText=true`);
+
+            xhttp2.onload = function (){
+
+                console.log(this);
+
+                if (xhttp2.status==200) {
+
+                    resolve(xhttp2.response);
+
+                }
+                else{
+
+                    reject(xhttp2.status);
+
+                }
+
+    }
+
+
+
+    xhttp2.send();
+
+
+
+    })
+
+    const weatherPromise = new Promise((resolve,reject)=>{
+
+            xhttpWeather = new XMLHttpRequest();
+
+                xhttpWeather.open("GET",`https://api.openweathermap.org/data/2.5/weather?q=${search.value}&appid=114ec643a112e43327c59d9900d3bb20&units=metric`);
+
+
+                xhttpWeather.onload = function () {
+
+                    if (xhttpWeather.status==200){
+                        resolve(xhttpWeather.response)
+                    }
+                    else{
+                        reject(xhttpWeather.status)
+                    }
+                    
+                }
+
+
+                xhttpWeather.send();
+
+
+                })
+                
+}
