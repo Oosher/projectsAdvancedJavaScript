883ea7a984a089a7ebf8f9d41139c2cb1306b154
Oosher <105375761+Oosher@users.noreply.github.com>
2022-12-11 18:27:39 +0200

    added an explanation on try and catch

--- class/fetchTryCatchAwait/fetchTryCatchAwait.js
+++ class/fetchTryCatchAwait/fetchTryCatchAwait.js
@@ -2,7 +2,7 @@
 
 //fetch
 
-const promise = fetch("https://restcountries.com/v3.1/all") ;
+const promise = fetch("https://restcountries.com/v3.1/all");
 
 promise
     .then((value)=>{
@@ -15,12 +15,43 @@ promise
     })
 
     .then((value)=>{
+
         console.log(value);
+
     })
 
     .catch((error)=>{
 
-
         console.log(error);
 
     })
+
+
+
+
+
+    //try & catch
+
+
+    /* 
+
+    let z = 5 + y ;
+
+    console.log("hello"); 
+
+    */
+
+
+    //when using try and catch the code wil not stop at the error but will continue and print the message instead 
+    try{
+
+        let x = 5 + y ;
+
+    }
+    
+    catch(error)
+    {
+
+        console.log(error);
+
+    }
