13bbff06fbad32391770b6a6753cd0950e682041
Oosher <105375761+Oosher@users.noreply.github.com>
2022-12-11 18:18:58 +0200

    Update fetchTryCatchAwait.js

--- class/fetchTryCatchAwait/fetchTryCatchAwait.js
+++ class/fetchTryCatchAwait/fetchTryCatchAwait.js
@@ -2,4 +2,25 @@
 
 //fetch
 
-const promise = fetch() 
+const promise = fetch("https://restcountries.com/v3.1/all") ;
+
+promise
+    .then((value)=>{
+        
+        //option 1
+        // return value.text();
+
+        return value.json();
+
+    })
+
+    .then((value)=>{
+        console.log(value);
+    })
+
+    .catch((error)=>{
+
+
+        console.log(error);
+
+    })
