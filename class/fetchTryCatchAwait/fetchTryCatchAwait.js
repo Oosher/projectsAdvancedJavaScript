7d638e2007facfcc80d2bf3f72b4305ed8427533
Oosher <105375761+Oosher@users.noreply.github.com>
2022-12-11 20:01:26 +0200

    fixed the fetch function  and added a refular way of creating the same function

--- class/fetchTryCatchAwait/fetchTryCatchAwait.js
+++ class/fetchTryCatchAwait/fetchTryCatchAwait.js
@@ -116,3 +116,90 @@ promise
 
 
     getCountriesAsync();
+
+
+    
+
+    //class task 
+
+
+
+    const arrayOfCountry = ["Israel","Canada","france","italy"];
+
+    function getCountryByNameWithFetch(countryName) {
+
+        return fetch(`https://restcountries.com/v3.1/name/${countryName}`);
+        
+    }
+
+    async function presentCountryNamesWithFetch(){
+
+        for (let i = 0; i < arrayOfCountry.length; i++) {
+            let countryToDisplay = await getCountryByNameWithFetch(arrayOfCountry[i]);
+
+        countryToDisplay= await countryToDisplay.json();
+
+        console.log(countryToDisplay[0].name.common);
+            
+        }
+
+
+    }
+
+
+    presentCountryNamesWithFetch();
+
+
+    //regular way
+
+
+    function getCountryByName(countryName) {
+     const newPromise = new Promise((resolve,reject)=>{
+
+            const xhttp = new XMLHttpRequest();
+
+            xhttp.open("get",`https://restcountries.com/v3.1//name/${countryName}`);
+
+            xhttp.onload =function(){
+                if (xhttp.status==200) {
+
+                    resolve(JSON.parse(xhttp.response));
+                    
+                }
+
+                else{
+                    
+                    reject(xhttp.status);
+
+                }
+            };
+            xhttp.send();
+
+        });
+
+        return newPromise;
+        
+    }
+
+    async function presentCountryNames(){
+
+        try{
+
+                for (let i = 0; i < arrayOfCountry.length; i++) {
+                    
+                    let countryToDisplay = await getCountryByName(arrayOfCountry[i]);
+
+                    console.log(countryToDisplay[0].name.common);
+                    
+                }
+
+        }
+
+        catch(error){
+            console.log(error);
+        }
+
+    }
+
+
+    presentCountryNames();
