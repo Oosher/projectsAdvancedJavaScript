6021fbaf46f5d794bf1eb02457692acd537e752c
Oosher <105375761+Oosher@users.noreply.github.com>
2022-12-14 17:53:18 +0200

    added a user class to import

---
+++ class/modulesImportExport/modulesImportExport.js
@@ -0,0 +1,11 @@
+
+import account from"./user.js";//import the default export from the other file the name dose not matter when using export default
+
+import { hello } from "./user.js";
+
+let myUser = new account("toshi ba",2313231);
+
+
+console.log(myUser);
+
+hello();
---
+++ class/modulesImportExport/user.js
@@ -0,0 +1,26 @@
+
+export default class User {
+
+    constructor (name,id){
+        this.name=name;
+
+        this.id=id;
+
+    }
+
+}
+
+
+
+
+
+
+export function hello() {
+
+    console.log(`Hello`);
+    
+}
+
+//option 2
+//export default User;
+//export { hello };
