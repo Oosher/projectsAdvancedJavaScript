0dd9ae26733ce9c3bd506c3d86cc8c3e51d6306e
Oosher <105375761+Oosher@users.noreply.github.com>
2022-12-14 21:08:32 +0200

    s

--- nodejs/intro/app.js
+++ nodejs/intro/app.js
@@ -1,2 +1,8 @@
 
+const fileSystem = require("fs");
+
+// destination.txt will be created or overwritten by default.
+fileSystem.copyFileSync('text.txt', 'destination.txt');
+console.log('source.txt was copied to destination.txt');
+
 console.log("hello");
