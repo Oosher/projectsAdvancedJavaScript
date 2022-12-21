b5973237c0d8cd4af707af7d69e69a63b449a39c
Oosher <105375761+Oosher@users.noreply.github.com>
2022-12-21 21:17:18 +0200

    added union types

--- nodejs/typeScript/typeScript.js
+++ nodejs/typeScript/typeScript.js
@@ -19,3 +19,54 @@ var myFunction2 = function (value) {
     return value * 100;
 };
 console.log(myFunction2(25));
+var arrayOfNames = ["yosh", "mosh", "kosh"];
+arrayOfNames.push("totes");
+/*
+will not work because its an array of string
+arrayOfNames.push(2133);
+arrayOfNames[0]= 23134;
+ */
+//combined array
+//array of strings and numbers
+var data = ["dasdas", "asdasdsad", 123122321];
+function towNumberSum(num1, num2) {
+    return num1 + num2;
+}
+console.log(towNumberSum(4, 9));
+//objects
+var student = {
+    fullName: "mimon",
+    age: 30
+};
+/*
+Type 'string' is not assignable to type 'number'
+
+student.age="43";
+ */
+/*
+Property 'grade' does not exist on type '{ fullName: string; age: number; }'
+
+student.grade =200;
+*/
+/*
+Property 'age' is missing in type '{ fullName: string; }' but required in type '{ fullName: string; age: number; }
+
+student = {
+    fullName:"shimo",
+    
+} */
+//explicit type
+var intType;
+var stringType;
+//explicit array type
+var newStringArray = [];
+newStringArray.push("hello");
+//explicit object type
+var newObject;
+newObject = {
+    fullName: "shimshi",
+    age: 57
+};
+//union types
+var object3;
+var newVariable;
--- nodejs/typeScript/typeScript.ts
+++ nodejs/typeScript/typeScript.ts
@@ -45,3 +45,103 @@ const myFunction2 = (value:number) =>{
 console.log(myFunction2(25));
 
 
+let arrayOfNames = ["yosh","mosh","kosh"];
+
+arrayOfNames.push("totes");
+/*
+will not work because its an array of string 
+arrayOfNames.push(2133);
+arrayOfNames[0]= 23134;
+ */
+
+//combined array
+
+//array of strings and numbers
+
+let data = ["dasdas","asdasdsad",123122321];
+
+
+function towNumberSum(num1:number,num2:number){
+
+    return num1+num2;
+
+}
+
+
+console.log(towNumberSum(4,9));
+
+//objects
+
+let student = {
+    fullName:"mimon",
+    age:30,
+};
+
+
+/*
+Type 'string' is not assignable to type 'number'
+
+student.age="43"; 
+ */
+
+
+/* 
+Property 'grade' does not exist on type '{ fullName: string; age: number; }'
+
+student.grade =200; 
+*/
+
+
+/* 
+Property 'age' is missing in type '{ fullName: string; }' but required in type '{ fullName: string; age: number; }
+
+student = {
+    fullName:"shimo",
+    
+} */
+
+
+//explicit type
+
+
+let intType:number;
+
+let stringType:string;
+
+
+//explicit array type
+
+let newStringArray:string[] = [];
+
+
+newStringArray.push("hello");
+
+
+//explicit object type
+
+let newObject:{
+
+    fullName:string;
+
+    age:number;
+
+}; 
+
+
+newObject ={
+
+    fullName:"shimshi",
+    age:57,
+    
+}
+
+
+//union types
+
+let object3 :{
+    fullName:string;
+    age:number;
+    id:number|string;
+}
+
+let newVariable:number | boolean;
