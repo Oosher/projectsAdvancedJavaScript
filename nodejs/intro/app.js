4a7e90aada9908c49007adfd4581bd6080cba7ae
Oosher <105375761+Oosher@users.noreply.github.com>
2022-12-21 18:24:41 +0200

    added some use  npm and some packeges

--- nodejs/intro/app.js
+++ nodejs/intro/app.js
@@ -1,8 +1,13 @@
 
-const fileSystem = require("fs");
+import superheroes from "superheroes";
+
+/* const fileSystem = require("fs");
 
 // destination.txt will be created or overwritten by default.
 fileSystem.copyFileSync('text.txt', 'destination.txt');
 console.log('source.txt was copied to destination.txt');
 
-console.log("hello");
+console.log("hello"); */
+
+
+console.log(superheroes.random());
