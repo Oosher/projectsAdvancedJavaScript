4a9d5f4a94709e39a77dad31ec43b9ae916c6d85
Oosher <105375761+Oosher@users.noreply.github.com>
2022-12-21 18:50:49 +0200

    added a new supervillans packege

--- nodejs/intro/app.js
+++ nodejs/intro/app.js
@@ -1,13 +1,15 @@
 
 import superheroes from "superheroes";
 
-/* const fileSystem = require("fs");
+import superVillains from "supervillains";
 
+import fileSystem from "fs";
 // destination.txt will be created or overwritten by default.
 fileSystem.copyFileSync('text.txt', 'destination.txt');
 console.log('source.txt was copied to destination.txt');
 
-console.log("hello"); */
+console.log("hello");
 
+console.log(superheroes.random()); 
 
-console.log(superheroes.random());
+console.log(superVillains.random());
