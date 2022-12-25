4ea1f7c396f9eae807d18202242161331ac1a0bc
Oosher <105375761+Oosher@users.noreply.github.com>
2022-12-25 18:08:02 +0200

    adde dsome functions and some type argumants

--- nodejs/typeScript2/typeScript.js
+++ nodejs/typeScript2/typeScript.js
@@ -12,3 +12,25 @@ var hello = function () {
     console.log("Hello");
 };
 hello();
+var newFunction;
+newFunction = function () {
+    console.log(a);
+};
+newFunction();
+//define type of args
+var addNums = function (num1, num2) {
+    console.log((num1 % num2));
+};
+addNums(7, 4);
+//define optional arguments
+var addNums2 = function (a, b, c) {
+    console.log(a / b);
+    console.log(c);
+};
+//c is optional so you don't have to input it when you call the function
+addNums2(45, 23);
+//default value
+var addNums3 = function (a, b, c) {
+    if (c === void 0) { c = 10; }
+    console.log(a / b * c);
+};
--- nodejs/typeScript2/typeScript.ts
+++ nodejs/typeScript2/typeScript.ts
@@ -35,3 +35,66 @@ let hello=()=>{
 };
 
 hello();
+
+
+
+let newFunction : Function ;
+
+
+
+
+newFunction = ()=>{
+
+    console.log(a);
+    
+
+};
+
+
+newFunction();
+
+
+
+//define type of args
+
+
+let addNums = (num1:number,num2:number)=>{
+
+    console.log((num1%num2));
+
+};
+
+
+
+addNums(7,4);
+
+
+
+
+//define optional arguments
+
+
+
+
+let addNums2 = (a:number,b:number,c?:number|string)=>{
+
+    console.log(a/b);
+
+    console.log(c);
+    
+}
+
+//c is optional so you don't have to input it when you call the function
+addNums2(45,23);
+
+
+
+
+
+//default value
+
+let addNums3 = (a:number,b:number,c:number = 10)=>{
+
+    console.log(a/b*c);
+    
+}
