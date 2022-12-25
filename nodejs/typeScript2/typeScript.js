757daf37f0686bbb658b3064f582968608c3f9bb
Oosher <105375761+Oosher@users.noreply.github.com>
2022-12-25 17:51:32 +0200

    aded an explanation on any type

---
+++ nodejs/typeScript2/typeScript.js
@@ -0,0 +1,14 @@
+//type - any
+var a;
+a = 435;
+a = "suiiii";
+a = true;
+var arrayOfAny = [];
+arrayOfAny.push(35);
+arrayOfAny.push("gffd");
+var anyStudent;
+//functions 
+var hello = function () {
+    console.log("Hello");
+};
+hello();
---
+++ nodejs/typeScript2/typeScript.ts
@@ -0,0 +1,37 @@
+
+//type - any
+
+let a: any;
+
+a =435;
+a="suiiii";
+a= true;
+
+
+
+let arrayOfAny : any [] = [];
+
+arrayOfAny.push(35);
+
+arrayOfAny.push("gffd");
+
+
+
+let anyStudent :{
+    name: any;
+    age:any;
+
+}
+
+
+
+//functions 
+let hello=()=>{
+
+    console.log("Hello");
+    
+
+
+};
+
+hello();
