43f99f384a9b486c34064c22a0f77492051bfc67
Oosher <105375761+Oosher@users.noreply.github.com>
2022-12-28 21:11:43 +0200

    added a serach by coin symbol

--- nodejs/typeScriptProject/public/typeScript.js
+++ nodejs/typeScriptProject/public/typeScript.js
@@ -2,6 +2,9 @@
 //crypto website
 //properties :symbol , last price ,volume ,price change percent
 //get data
+const cryptoTable = document.querySelector("#newCryptoCoin");
+const searchByCoinName = document.querySelector("#searchBtn");
+const coinSymbol = document.querySelector("#currency-name");
 class CryptoCoin {
     constructor(symbol, lastPrice, volume, priceChangePercent) {
         this.symbol = symbol;
@@ -10,8 +13,9 @@ class CryptoCoin {
         this.priceChangePercent = priceChangePercent;
     }
 }
+//option 1
 function getData() {
-    const cryptoPromise = new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         const cryptoCoins = new XMLHttpRequest();
         cryptoCoins.open("GET", "https://api2.binance.com/api/v3/ticker/24hr");
         cryptoCoins.onload = function () {
@@ -24,11 +28,49 @@ function getData() {
         };
         cryptoCoins.send();
     });
-    cryptoPromise.then((response) => {
+    /*     cryptoPromise.then((response:any)=>{
+    
         console.log(response);
+    
+        }).catch((status:any)=>{
+    
+    
+        }) */
+}
+let coinsArray = [];
+function createCryptoArray() {
+    getData().then((response) => {
+        console.log(response);
+        coinsArray = response.map((coin) => {
+            return new CryptoCoin(coin.symbol, +coin.lastPrice, +coin.volume, +coin.priceChangePercent);
+        });
+        displayData(coinsArray);
     }).catch((status) => {
+        console.log(status);
     });
 }
-getData();
+createCryptoArray();
+//option 2 fetch
+function getData2() {
+}
+;
+function displayData(array) {
+    array.map((coin) => {
+        let newCryptoTable = document.createElement("tr");
+        newCryptoTable.innerHTML = `<th>${coin.symbol}</th>
+                                <th>${coin.lastPrice}</th>
+                                <th>${coin.volume}</th>
+                                <th>${coin.priceChangePercent}</th>`;
+        newCryptoTable.className = "newCryptoTable";
+        cryptoTable === null || cryptoTable === void 0 ? void 0 : cryptoTable.appendChild(newCryptoTable);
+    });
+}
+//search by coin name name 
+searchByCoinName === null || searchByCoinName === void 0 ? void 0 : searchByCoinName.addEventListener("click", () => {
+    cryptoTable.innerHTML = "";
+    displayData(coinsArray.filter((coin) => {
+        return coin.symbol == coinSymbol.value;
+    }));
+});
 const newCoin = new CryptoCoin("abah", 123, 12, 3.6);
 console.log(newCoin);
--- nodejs/typeScriptProject/src/typeScript.ts
+++ nodejs/typeScriptProject/src/typeScript.ts
@@ -8,6 +8,13 @@
 
 //get data
 
+
+const cryptoTable = document.querySelector("#newCryptoCoin")!;
+
+const searchByCoinName = document.querySelector("#searchBtn");
+
+const coinSymbol = document.querySelector("#currency-name") as HTMLInputElement;
+
 class CryptoCoin{
 
     symbol: string;
@@ -34,11 +41,11 @@ class CryptoCoin{
 
 
 } 
-
+//option 1
 function getData(){
 
 
-    const cryptoPromise = new Promise ((resolve,reject)=>{
+    return new Promise ((resolve,reject)=>{
 
 
         const cryptoCoins = new XMLHttpRequest();
@@ -64,21 +71,82 @@ function getData(){
     });
 
 
-    cryptoPromise.then((response:any)=>{
+
+/*     cryptoPromise.then((response:any)=>{
 
     console.log(response);
 
     }).catch((status:any)=>{
 
 
-    })
+    }) */
 
 
 }
 
-getData();
+let coinsArray: CryptoCoin []= [];
+
+function createCryptoArray(){
+
+    getData().then((response:any)=>{
+
+        console.log(response);
+
+        coinsArray = response.map((coin:any)=>{
+            return new CryptoCoin(coin.symbol,+coin.lastPrice,+coin.volume,+coin.priceChangePercent)
+        })
+
+        displayData(coinsArray);
+
+        }).catch((status:any)=>{
+
+            console.log(status);
+            
+
+        });
+    
+} 
+
+createCryptoArray();
 
 
+//option 2 fetch
+
+function getData2(){
+
+};
+
+
+function displayData(array:CryptoCoin[]){
+
+    array.map((coin:any)=>{
+
+        let newCryptoTable = document.createElement("tr");
+        
+        newCryptoTable.innerHTML=`<th>${coin.symbol}</th>
+                                <th>${coin.lastPrice}</th>
+                                <th>${coin.volume}</th>
+                                <th>${coin.priceChangePercent}</th>`;
+        newCryptoTable.className="newCryptoTable";
+
+        cryptoTable?.appendChild(newCryptoTable);
+
+    })
+        
+}
+
+//search by coin name name 
+
+
+searchByCoinName?.addEventListener("click",()=>{
+    cryptoTable.innerHTML="";
+    
+    displayData(coinsArray.filter((coin)=>{
+        return coin.symbol == coinSymbol.value;
+    }));
+
+    
+})
 
 const newCoin = new CryptoCoin("abah",123,12,3.6);
 
