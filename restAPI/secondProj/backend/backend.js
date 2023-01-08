7c2af296f919a85fd96c21e24d79760f2caa74bf
Oosher <105375761+Oosher@users.noreply.github.com>
2023-01-08 18:44:12 +0200

    added another get function to the api

--- restAPI/secondProj/backend/backend.js
+++ restAPI/secondProj/backend/backend.js
@@ -16,10 +16,18 @@ api.use(
 
 api.get("/",function(req,res){
 
-    res.send(randObj);
+    res.send("nuds");
 
 });
 
+api.get("/randobj/:objName",function(req,res){
+
+    res.send(randObj[randObj.findIndex((obj)=>{
+        return Object.keys(obj) == req.params.objName;
+    })]);
+
+    
+});
 api.listen(3000,function(){
     console.log("server is working");
 })
