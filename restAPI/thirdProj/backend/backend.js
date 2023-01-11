25cd29640901dc4c64ed3279c69aa458c1a4ee0c
Oosher <105375761+Oosher@users.noreply.github.com>
2023-01-11 20:52:57 +0200

    added a sing up form and a get and post methods to the api

--- restAPI/thirdProj/backend/backend.js
+++ restAPI/thirdProj/backend/backend.js
@@ -1,52 +1,74 @@
 
-import express from "express";
+/* import express from "express"; */
 
-import cors from "cors";
+const express =require("express");
 
-import defaultRandObj  from "./dataBase.js";
+/* import cors from "cors"; */
 
-import bodyParser from "body-parser";
+const cors =require("cors");
 
-import LocalStorage from "node-localstorage";
+
+
+/* import bodyParser from "body-parser"; */
+
+const bodyParser = require("body-parser");
+
+/* import LocalStorage from "node-localstorage"; */
 
 
 
 const api = express();
 
-const localStorage = new LocalStorage.LocalStorage('./scratch');
-
-let randObj = defaultRandObj;
-
-if(localStorage.getItem("savedArray")!=""){
-
-    randObj = JSON.parse(localStorage.getItem("savedArray"));
-
-}
+/* const localStorage = new LocalStorage.LocalStorage('./scratch'); */
 
 
-save();
+const localStorage = require("node-localstorage").LocalStorage('./scratch')
+
+
 
 api.use(
     cors({ 
         origin:"*",
     }),
     bodyParser.json(),
-
+    express.static("public"),
 );
 
 
 api.get("/",(req,res)=>{
-
-    res.send("hallo");
+    console.log(__dirname);
+    res.sendFile(__dirname +`/public/signUp.html`);
 
 });
+
+api.get("/SUCCESS",(req,res)=>{
+    console.log(__dirname);
+    res.sendFile(__dirname +`/public/success.html`);
+
+});
+
+api.get("/failure",(req,res)=>{
+    console.log(__dirname);
+    res.sendFile(__dirname +`/public/failure.html`);
+
+});
+
+
+api.post("/",(req,res)=>{
+
+    console.log(req.body);
+
+    res.send(req.body);
+
+});
+
 
 api.listen(3000,function(){
     console.log("server is working");
 })
 
-function save (){
+/* function save (){
 
     localStorage.setItem("savedArray",JSON.stringify(randObj));
 
-}
+} */
