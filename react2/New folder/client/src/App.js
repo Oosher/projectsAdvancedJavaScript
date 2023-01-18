7040acdfc190668a2690739df52f4ebbcd711ac3
Oosher <105375761+Oosher@users.noreply.github.com>
2023-01-18 19:08:04 +0200

    added a logic to the component

--- react2/New folder/client/src/App.js
+++ react2/New folder/client/src/App.js
@@ -1,10 +1,10 @@
-
+import PageHeader from './components/header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-  
+      <PageHeader/>
     </div>
   );
 }
--- react2/New folder/client/src/components/header.jsx
+++ react2/New folder/client/src/components/header.jsx
@@ -2,7 +2,15 @@
 const PageHeader  = ()=>{
 
 
-    return <h2>This is the page header</h2>
+    let sum =5*7;
+
+    console.log(sum);
+
+    //cannot return more than 1 component but its is posibble by wrapping it in a <div></div> or <?></>
+    return <>
+    <h2>This is the page header</h2>
+    <h1>brutrue {sum}</h1>
+    </>;
 
 }
 
