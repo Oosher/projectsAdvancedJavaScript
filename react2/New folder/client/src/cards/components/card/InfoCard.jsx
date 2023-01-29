90bd9c5c84a163455c0c80a72ab9e6506bf12688
Oosher <105375761+Oosher@users.noreply.github.com>
2023-01-29 18:13:50 +0200

    added some click function to the card

--- react2/New folder/client/src/cards/components/card/InfoCard.jsx
+++ react2/New folder/client/src/cards/components/card/InfoCard.jsx
@@ -1,4 +1,4 @@
-import { Card, CardMedia, CardHeader, IconButton, Divider, CardContent, Typography } from '@mui/material'
+import { Card, CardMedia, CardHeader, IconButton, Divider, CardContent, Typography, CardActions, CardActionArea } from '@mui/material'
 import React from 'react'
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
@@ -8,40 +8,42 @@ import { Box } from '@mui/system';
 
 export default function InfoCard() {
     return <Card sx={{width:"15vw",margin:"0 auto", padding:"10px",textAlign:"left"}}>
-    <CardMedia component="img" title="" image="assets/images/business-card-top-imag.jpg" />
-    <CardHeader
-        title="tot"
-        subheader="num"
-        
-    />
-    <Divider/>
-    <CardContent>
-    <Typography variant="body2" color="initial">Phone: 05050050</Typography>
-    <Typography variant="body2" color="initial">Address: sad a2 asd </Typography>
-    <Typography variant="body2" color="initial">Card NUmber: 23444444</Typography>
-    </CardContent>
-    <Box disableSpacing sx={{display:"flex",justifyContent:"space-between",position:"rela",marginBottom:"0px",paddingBottom:"0px"}}>
-        <Box>
-        <IconButton>
-        <DeleteIcon/>
-        </IconButton>
+        <CardActionArea>
+            <CardMedia component="img" title="" image="assets/images/business-card-top-imag.jpg" />
+            <CardHeader
+                title="tot"
+                subheader="num"
+                
+            />
+            <Divider/>
+            <CardContent>
+            <Typography variant="body2" color="initial"><strong>Phone:</strong> 05050050</Typography>
+            <Typography variant="body2" color="initial"><strong>Address:</strong> sad a2 asd </Typography>
+            <Typography variant="body2" color="initial"><strong>Card Number:</strong> 23444444</Typography>
+            </CardContent>
+        </CardActionArea>
+        <CardActions disableSpacing sx={{display:"flex",justifyContent:"space-between",position:"rela",marginBottom:"0px",paddingBottom:"0px"}}>
+            <Box>
+            <IconButton aria-label='Delete Button'>
+            <DeleteIcon/>
+            </IconButton>
 
-        <IconButton>
-        <EditIcon/>
-        </IconButton>
+            <IconButton aria-aria-label='Edit Button'>
+            <EditIcon/>
+            </IconButton>
 
-        </Box>  
-        
-        <Box>
-        <IconButton>
-        <PhoneIcon/>
-        </IconButton>
+            </Box>  
+            
+            <Box>
+            <IconButton aria-aria-label='Phone Button'>
+            <PhoneIcon/>
+            </IconButton>
 
-        <IconButton> 
-        <FavoriteIcon/>
-        </IconButton>
-        </Box>
-    </Box>
+            <IconButton aria-aria-label='Add To Favorites'> 
+            <FavoriteIcon/>
+            </IconButton>
+            </Box>
+        </CardActions>
     </Card>;
     
 }
